1892b04453045c052ee5906f2863b0959b15879c
BlueMarth <hubert.lcr@gmail.com>
2024-04-22 14:43:48 +0800

    Lesson update

--- 1_Getting_Started/1.1_Windows.py
+++ 1_Getting_Started/1.1_Windows.py
@@ -1,20 +1,35 @@
-# import package pygame 
-import pygame 
-  
-# Form screen with 400x400 size 
-# and with resizable 
-screen = pygame.display.set_mode((400, 400),  
-                                 pygame.RESIZABLE) 
-  
-# set title 
-pygame.display.set_caption('Resizable') 
-  
-# run window 
-running = True
-while running: 
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: 
-            running = False
-  
-# quit pygame after closing window 
-pygame.quit() 
+import pygame
+
+pygame.init()
+
+# create window, get size, close window
+screen = pygame.display.set_mode()
+x, y = screen.get_size()
+pygame.display.quit()
+
+# create new resizable window with half the screen sizes
+screen = pygame.display.set_mode((int(x/2), int(y/2)), pygame.RESIZABLE)
+
+# set colour
+colour = (144, 164, 174)
+screen.fill(colour)
+pygame.display.flip()
+
+#set title & icon
+pygame.display.set_caption('G4G')
+icon = pygame.image.load('github_icon.png')
+pygame.display.set_icon(icon)
+
+# main loop
+while True:
+
+
+
+
+
+
+    # close window button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
