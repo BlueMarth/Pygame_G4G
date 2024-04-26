5157def55b5b603f8bc8b4e5ace700fc4c36c62c
BlueMarth <hubert.lcr@gmail.com>
2024-04-26 17:25:26 +0800

    Update 6.4.2_space.py

--- 6_Advanced/6.4.2_space.py
+++ 6_Advanced/6.4.2_space.py
@@ -6,18 +6,20 @@ pygame.init()
 SIZE = [480, 270]
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Space")
+fps = 60
 
+color_list = ["white", "gray", "gray25", "gray50", "gray75",
+              "darkmagenta", "darkblue", "darkred"]
 stars = []
 
-for i in range(250):
-    mini_x = random.random(0,1)-0.5
-    mini_y = random.random(0,1)-0.5
-    x = SIZE[0]//2 + mini_x
-    y = SIZE[1]//2 + mini_y
+for i in range(25):
+    x = SIZE[0]//2 + random.random()
+    y = SIZE[1]//2 + random.random()
     radius = random.randrange(1, 3)
     angle = random.randrange(0, 359)
     speed = radius
-    stars.append([x,y,radius,angle,speed])
+    color = random.choice(color_list)
+    stars.append([x,y,radius,angle,speed,color])
 
 clock = pygame.time.Clock()
 
@@ -29,37 +31,35 @@ while not done:
             done = True
 
     screen.fill("black")
-
+    
     for i in range(len(stars)):
+        # compute distance of star from screen center
         dist = math.sqrt((stars[i][0]-SIZE[0]//2)**2
                          + (stars[i][1]-SIZE[1]//2)**2)
-        if dist > stars[i][2]:
-            pygame.draw.circle(screen, "gray60",
+        
+        # draw star only if close enough (further away from screen center)
+        if dist > stars[i][2] * 2:
+            pygame.draw.circle(screen, stars[i][5],
                            [stars[i][0],stars[i][1]], stars[i][2])
         
         # compute displacement angle
         dx = (math.cos(stars[i][3] / 180 * math.pi))
         dy = (math.sin(stars[i][3] / 180 * math.pi))
         
-        # the closer the faster
-        
-        stars[i][4] = dist * stars[i][2] / 30
-        
+        # the closer the faster and bigger
         stars[i][0] += stars[i][4] * dx
         stars[i][1] += stars[i][4] * dy
-        
-        if dist > stars[i][2] + 2:
-            pygame.draw.circle(screen, "gray40",
-                           [stars[i][0],stars[i][1]], stars[i][2])
+        stars[i][4] = dist * stars[i][2] / 40
+        stars[i][2] += math.sqrt(dist) // 60
 
+        # reset star position once outside of window
         if (((stars[i][0] <= 0) or (stars[i][0] >= SIZE[0]))
             or ((stars[i][1] <= 0) or (stars[i][1] >= SIZE[1]))):
-            stars[i][0] = SIZE[0]//2
-            stars[i][1] = SIZE[1]//2
+            stars[i][0] = SIZE[0]//2 + random.random()
+            stars[i][1] = SIZE[1]//2 + random.random()
+            stars[i][2] = random.randrange(1,3)
             stars[i][3] = random.randrange(0,359)
-        
-        
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(fps)
 pygame.quit
