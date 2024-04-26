5f381917e38cd3987ca6b1d97e294b22ed67f372
BlueMarth <hubert.lcr@gmail.com>
2024-04-26 13:16:57 +0800

    Update 6.3_Sprite.py

--- 6_Advanced/6.3_Sprite.py
+++ 6_Advanced/6.3_Sprite.py
@@ -1,9 +1,11 @@
 import pygame
+pygame.init()
 
-COLOR_KEY = "red"
+COLOR = "red"
 SCREEN_COLOR = "darkslategray"
-SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, color, height, width):
@@ -11,15 +13,13 @@ class Sprite(pygame.sprite.Sprite):
 
         self.image = pygame.Surface([width,height])
         self.image.fill(SCREEN_COLOR)
-        self.image.set_colorkey(COLOR_KEY)
+        self.image.set_colorkey(COLOR)
 
-        pygame.draw.rect(self.image,COLOR_KEY,
+        pygame.draw.rect(self.image,color,
                          pygame.Rect(0, 0, width, height))
         
         self.rect = self.image.get_rect()
 
-
-pygame.init()
 
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
@@ -27,4 +27,23 @@ pygame.display.set_caption("Creatiing Sprite")
 
 all_sprites_list = pygame.sprite.Group()
 
-object_ = Sprite("red", )
+object_ = Sprite("red", 20, 30)
+object_.rect.x = 200
+object_.rect.y = 300
+
+all_sprites_list.add(object_)
+
+exit = True
+clock = pygame.time.Clock()
+
+while exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = False
+    all_sprites_list.update()
+    screen.fill(SCREEN_COLOR)
+    all_sprites_list(screen)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
